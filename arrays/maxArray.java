2447483cf8bea2cff36ded248722cbb5271a3a93
amitbishnoii <amitbishnoi29012@gmail.com>
2025-10-06 21:55:23 +0530

    add program to find maximum item in an array

--- arrays/maxArray.java
+++ arrays/maxArray.java
@@ -1,12 +1,18 @@
+import java.util.Arrays;
+
 public class maxArray {
     public static void main(String[] args) {
-        int[] arr = {1, 34, 21, 32, 55};
-        max(arr);
+        int[] arr = {1, 34, 21, 1212, 55};
+        System.out.println(max(arr));
     }
 
-    static void max(int[] arr) {
+    static int max(int[] arr) {
+        int current_max = 0;
         for (int i : arr) {
-            
+            if (i > current_max) {
+                current_max = i;
+            }
         }
+        return current_max;
     }
 }
